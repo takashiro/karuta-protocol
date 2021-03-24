4a3d0290692ae9df5f6e250521106303705d2ff0
Kazuichi Takashiro <takashiro@qq.com>
2021-03-24 22:17:19 +0800

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'airbnb-base',
+		'plugin:@typescript-eslint/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: [
+		'@typescript-eslint',
+	],
+	rules: {
+		indent: [
+			'error',
+			'tab',
+		],
+		'no-shadow': 'off',
+		'no-tabs': 'off',
+		'linebreak-style': 'off',
+	},
+};
