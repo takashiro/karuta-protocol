40561cc43f794f6a1d22d47f49c839d3f88e89cd
Kazuichi Takashiro <takashiro@qq.com>
2021-06-13 20:52:42 +0800

    Remove class Packet

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { default as Context } from './Context';
 export { default as ContextListener } from './ContextListener';
 export { default as Method } from './Method';
-export { default as Packet } from './Packet';
